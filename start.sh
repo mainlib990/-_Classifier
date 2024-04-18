52cc39689e2a2e145459537d7a4cda2dce312f8b
Anonymous <none@none.com>
2024-04-18 14:57:10 +0900

    fix: Convert CRLF to LF

--- start.sh
+++ start.sh
@@ -1,22 +1,25 @@
 #! /bin/sh
 
 rm -rf "tmp"
+mkdir -p "tmp"
 
 index_file="numbers.txt"
 
+tr -d '\015' < "$index_file" > "tmp/$index_file"
+
 # pick
 echo "picking..."
-./bin/picker.sh "$index_file" "resources/본문" "tmp/picked"
+./bin/picker.sh "tmp/$index_file" "resources/본문" "tmp/picked"
 
 # head
 echo "head mapping..."
-./bin/head_mapper.sh "$index_file" "tmp/picked" "resources/간지" "tmp/head_mapped"
+./bin/head_mapper.sh "tmp/$index_file" "tmp/picked" "resources/간지" "tmp/head_mapped"
 
 # nonhead
 echo "nonhead mapping..."
-./bin/nonhead_mapper.sh "$index_file" "tmp/picked" "tmp/nonhead_mapped"
+./bin/nonhead_mapper.sh "tmp/$index_file" "tmp/picked" "tmp/nonhead_mapped"
 
 # result
 echo "zipping..."
-count=$(wc -l < $index_file)
+count=$(wc -l < "tmp/$index_file")
 ./bin/zipper.sh $count "tmp/head_mapped" "tmp/nonhead_mapped" "result"
